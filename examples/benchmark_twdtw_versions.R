ba99561b08dd36f59bcbad706087ce121037978d
vwmaus <vwmaus1@gmail.com>
2023-06-30 23:14:56 +0200

    Fixes benchmark example

--- examples/benchmark_twdtw_versions.R
+++ examples/benchmark_twdtw_versions.R
@@ -31,22 +31,24 @@ twdtw_call <- function(x = ts_x,
                        y = ts_y,
                        time_cycle_length = 'year',
                        time_cycle_scale = 'day',
-                       time_weight_par = c(steepness = -0.1, midpoint = 50), ...){
+                       time_weight_par = c(steepness = 0.1, midpoint = 50), ...){
   twdtw(x = x,
         y = y,
         time_cycle_length = time_cycle_length,
         time_cycle_scale = time_cycle_scale,
-        time_weight_par = time_weight_par, ...)
+        time_weight = time_weight_par, ...)
 }
+
+tw_r_fun <- function(x,y) x + 1.0 / (1.0 + exp(-0.1 * (y - 50)))
 
 # Benchmark default TWDTW call
 benchmark(
   twdtw_f90        = twdtw_call(version = 'f90'),
-  twdtw_f90_fun    = twdtw_call(version = 'f90', time_weight_fun = function(x,y,tw1,tw2) x + 1.0 / (1.0 + exp(tw1 * (y - tw2)))),
+  twdtw_f90_fun    = twdtw_call(version = 'f90', time_weight = tw_r_fun),
   twdtw_f90goto    = twdtw_call(version = 'f90goto'),
   twdtw_cpp        = twdtw_call(version = 'cpp'),
   twdtw_f90_lb     = twdtw_call(version = 'f90', max_elapsed = 30),
-  twdtw_f90_fun_lb = twdtw_call(version = 'f90', max_elapsed = 30, time_weight_fun = function(x,y,tw1,tw2) x + 1.0 / (1.0 + exp(tw1 * (y - tw2)))),
+  twdtw_f90_fun_lb = twdtw_call(version = 'f90', max_elapsed = 30, time_weight = tw_r_fun),
   twdtw_f90goto_lb = twdtw_call(version = 'f90goto', max_elapsed = 30),
   twdtw_cpp_lb     = twdtw_call(version = 'cpp', max_elapsed = 30),
   dtw_cpp          = dtw(ts_x[,c(2,2:4)], ts_y[,c(2,2:4)], distance.only = TRUE), # does not support time dimension
@@ -55,11 +57,11 @@ benchmark(
 
 # All calls must return the same result
 twdtw_f90     = twdtw_call(version = 'f90')
-twdtw_f90_fun = twdtw_call(version = 'f90', time_weight_fun = function(x,y,tw1,tw2) x + 1.0 / (1.0 + exp(tw1 * (y - tw2))))
+twdtw_f90_fun = twdtw_call(version = 'f90', time_weight = tw_r_fun)
 twdtw_f90goto = twdtw_call(version = 'f90goto')
 twdtw_cpp     = twdtw_call(version = 'cpp')
 twdtw_f90_lb     = twdtw_call(version = 'f90', max_elapsed = 30)
-twdtw_f90_fun_lb = twdtw_call(version = 'f90', max_elapsed = 30, time_weight_fun = function(x,y,tw1,tw2) x + 1.0 / (1.0 + exp(tw1 * (y - tw2))))
+twdtw_f90_fun_lb = twdtw_call(version = 'f90', max_elapsed = 30, time_weight = tw_r_fun)
 twdtw_f90goto_lb = twdtw_call(version = 'f90goto', max_elapsed = 30)
 twdtw_cpp_lb     = twdtw_call(version = 'cpp', max_elapsed = 30)
 
