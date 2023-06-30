f240c428d0ba154c211a07286fd8d24c28023a31
vwmaus <vwmaus1@gmail.com>
2023-06-30 10:50:16 +0200

    Correct examples and benchmark

--- examples/benchmark_twdtw_versions.R
+++ examples/benchmark_twdtw_versions.R
@@ -1,55 +1,60 @@
 library(twdtw)
 library(dtw)
+library(ggplot2)
 
-n <- 500
-t <- seq(0, 4*pi, length.out = n)
+n <- 23
+t <- seq(0, pi, length.out = n)
+d <- seq(as.Date('2020-09-01'), length.out = n, by = "15 day")
 
-x <- data.frame(date = seq(as.Date("2020-01-01"), by = "day", length.out = n),
-             value = sin(t)*2 + runif(n))
+# Define funcrion to normalize time series
+fun_range <- function(x) {
+  (x - min(x)) / (max(x) - min(x))
+}
 
-y <- data.frame(date = seq(as.Date("2020-01-15"), by = "day", length.out = n),
-             value = sin(t)*2 + runif(n))
+# Create time series with 4 dimensions
+ts_x <- data.frame(time = d,
+                   v1 = sin(t)*2 + runif(n),
+                   v2 = sin(t)*3 + runif(n),
+                   v3 = sin(t)*4 + runif(n),
+                   v4 = sin(t)*5 + runif(n))
 
-plot(x, type = "l")
-lines(y, col = "red")
+# Create a second time series with 4 dimensions an shift time by 30 days
+ts_y <- data.frame(time = d + 30,
+                   v1 = sin(t)*2 + runif(n),
+                   v2 = sin(t)*3 + runif(n),
+                   v3 = sin(t)*4 + runif(n),
+                   v4 = sin(t)*5 + runif(n))
+
+# Define TWDTW call
+twdtw_call <- function(x = ts_x,
+                       y = ts_y,
+                       time_cycle_length = 'year',
+                       time_cycle_scale = 'day',
+                       time_weight_par = c(steepness = -0.1, midpoint = 50), ...){
+  twdtw(x = x,
+        y = y,
+        time_cycle_length = time_cycle_length,
+        time_cycle_scale = time_cycle_scale,
+        time_weight_par = time_weight_par, ...)
+}
 
 rbenchmark::benchmark(
-  twdtw_f90     = twdtw(x, y, tw = c(-.1, 50), version = 'f90'),
-  twdtw_f90goto = twdtw(x, y, tw = c(-.1, 50), version = 'f90goto'),
-  twdtw_cpp     = twdtw(x, y, tw = c(-.1, 50), version = 'cpp'),
-  dtw_cpp       = dtw(x$value, y$value, distance.only = TRUE), # does not support time dimension
-  replications = 100
+  twdtw_f90     = twdtw_call(version = 'f90'),
+  twdtw_f90_fun = twdtw_call(version = 'f90', time_weight_fun = function(x,y,tw1,tw2) x + 1.0 / (1.0 + exp(tw1 * (y - tw2)))),
+  twdtw_f90goto = twdtw_call(version = 'f90goto'),
+  twdtw_cpp     = twdtw_call(version = 'cpp'),
+  dtw_cpp       = dtw(ts_x[,c(2,2:4)], ts_y[,c(2,2:4)], distance.only = TRUE), # does not support time dimension
+  replications = 1000
 )
+
+# All calls must return the same result
+twdtw_f90     = twdtw_call(version = 'f90')
+twdtw_f90_fun = twdtw_call(version = 'f90', time_weight_fun = function(x,y,tw1,tw2) x + 1.0 / (1.0 + exp(tw1 * (y - tw2))))
+twdtw_f90goto = twdtw_call(version = 'f90goto')
+twdtw_cpp     = twdtw_call(version = 'cpp')
 
 # Check results
-f90     = twdtw(x, y, tw = c(-.1, 50), version = 'f90')
-f90goto = twdtw(x, y, tw = c(-.1, 50), version = 'f90goto')
-cpp     = twdtw(x, y, tw = c(-.1, 50), version = 'cpp')
+identical(twdtw_f90, twdtw_f90_fun)
+identical(twdtw_f90_fun, twdtw_f90goto)
+identical(twdtw_f90goto, twdtw_cpp)
 
-identical(f90, f90goto)
-identical(f90, cpp)
-
-n <- 20
-t <- seq(0, pi, length.out = n)
-
-x <- data.frame(date = seq(as.Date("2020-01-01"), by = "day", length.out = n),
-                b1 = sin(t)*2 + runif(n),
-                b2 = sin(t)*2 + runif(n),
-                b3 = sin(t)*2 + runif(n),
-                b4 = sin(t)*2 + runif(n))
-
-y <- data.frame(date = seq(as.Date("2020-01-05"), by = "day", length.out = n),
-                b1 = sin(t)*2 + runif(n),
-                b2 = sin(t)*2 + runif(n),
-                b3 = sin(t)*2 + runif(n),
-                b4 = sin(t)*2 + runif(n))
-
-rbenchmark::benchmark(
-  twdtw_f90     = twdtw(x, y, tw = c(-.1, 50),
-                        tw_r = function(dist,td,tw1,tw2) dist + 1.0 / (1.0 + exp(-0.1 * (td - 50)))),
-  twdtw_f90_tw  = twdtw(x, y, tw = c(-.1, 50), version = 'f90'),
-  twdtw_f90goto = twdtw(x, y, tw = c(-.1, 50), version = 'f90goto'),
-  twdtw_cpp     = twdtw(x, y, tw = c(-.1, 50), version = 'cpp'),
-  dtw_cpp       = dtw(x[,c(2,2:5)], y[,c(2,2:5)], distance.only = TRUE), # does not support time dimension
-  replications = 100
-)
